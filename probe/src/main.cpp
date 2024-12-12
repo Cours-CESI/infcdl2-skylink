13f197698825f775b1c318774d8258d635a5df91
OGR-67 <roosolivier@gmail.com>
2024-12-12 14:51:18 +0100

    probe: add I2C address scanner

--- probe/src/main.cpp
+++ probe/src/main.cpp
@@ -1,22 +1,56 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 
-// put function declarations here:
-void sayHello(String greetings);
+Adafruit_BME280 bme;
 
 void setup()
 {
   Serial.begin(9600);
-  // put your setup code here, to run once:
-  sayHello("Hello");
+  while (!Serial)
+    ; // Wait serail port
+
+  Wire.begin(0, 2); // GPIO0 = SDA, GPIO2 = SCL
+
+  delay(1000);
+  Serial.println("\nScan I2C en cours...");
+
+  int foundDevices = 0;
+  for (byte address = 1; address < 127; address++)
+  {
+    Wire.beginTransmission(address);
+    if (Wire.endTransmission() == 0)
+    {
+      Serial.print("Périphérique trouvé à l'adresse 0x");
+      Serial.println(address, HEX);
+      foundDevices++;
+    }
+  }
+
+  if (foundDevices == 0)
+  {
+    Serial.println("Aucun périphérique I2C détecté.");
+  }
+  else
+  {
+    Serial.print("Nombre total de périphériques détectés : ");
+    Serial.println(foundDevices);
+  }
+
+  unsigned status;
+  status = bme.begin(0x76);
+
+  if (!status)
+  {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1)
+      ;
+  }
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-void sayHello(String greetings)
-{
-  Serial.println(greetings);
+  delay(1000);
+  Serial.println(bme.readTemperature());
 }
