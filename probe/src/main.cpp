eb0e505381c326a659f178767bbf4b2af336ca65
OGR-67 <roosolivier@gmail.com>
2024-12-12 15:56:42 +0100

    probe: add wifi and lcd with sensor's data displayed

--- probe/src/main.cpp
+++ probe/src/main.cpp
@@ -2,8 +2,14 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+#include <LiquidCrystal_I2C.h>
+#include <ESP8266WiFi.h>
+
+const char *ssid = "SSID";
+const char *password = "PASSWORD";
 
 Adafruit_BME280 bme;
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void scanI2CDevices()
 {
@@ -36,8 +42,33 @@ std::tuple<float, float, float> readBME280Data()
 {
   float temperature = bme.readTemperature();
   float humidity = bme.readHumidity();
-  float pressure = bme.readPressure() / 100.0F; // hPa conversion
+  float pressure = bme.readPressure() / 100.0F; // Conversion en hPa
   return std::make_tuple(temperature, humidity, pressure);
+}
+
+void initWiFi()
+{
+  WiFi.begin(ssid, password);
+  Serial.print("Connexion au Wi-Fi");
+
+  // Boucle jusqu'à connexion
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("\nConnecté au Wi-Fi");
+  Serial.println(WiFi.localIP());
+}
+
+void initLCD()
+{
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("TP CESI");
+  lcd.setCursor(0, 1);
+  lcd.print("Bloc 2");
 }
 
 void setup()
@@ -45,6 +76,8 @@ void setup()
   Serial.begin(9600);
   while (!Serial)
     ;
+  initWiFi();
+
   Wire.begin(0, 2); // GPIO0 = SDA, GPIO2 = SCL
 
   delay(1000);
@@ -57,6 +90,9 @@ void setup()
     while (1)
       ;
   }
+  Serial.println("BME280 initialisé avec succès !");
+
+  initLCD();
 }
 
 void loop()
@@ -75,4 +111,17 @@ void loop()
   Serial.print("Pression : ");
   Serial.print(pressure);
   Serial.println(" hPa");
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("T:");
+  lcd.print(temperature);
+  lcd.print("C H:");
+  lcd.print(humidity);
+  lcd.print("%");
+
+  lcd.setCursor(0, 1);
+  lcd.print("P:");
+  lcd.print(pressure);
+  lcd.print("hPa");
 }
