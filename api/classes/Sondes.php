5ed6e7c1c2d4860499485c0d382863470eceecb3
Téo <teomullerheddar@gmail.com>
2025-01-27 16:15:25 +0100

    Sonde add methode

--- api/classes/Sondes.php
+++ api/classes/Sondes.php
@@ -16,19 +16,36 @@ class Sondes
     public function post()
     {
         $data = json_decode(file_get_contents("php://input"), true);
-        $path = getCleanedPath();
-        switch ($path) {
-            case 'create':
-                try {
-                    $pdo = getDatabaseConnection();
-                    $sql = "INSERT INTO sondes (ip, name) VALUES ('" . $data['ip'] . "', '" . $data['name'] . "')";
-                    $pdo->exec($sql);
-                } catch (PDOException $e) {
-                    echo json_encode(['error' => $e->getMessage()]);
-                }
-                break;
+        try {
+            $pdo = getDatabaseConnection();
+            $sql = "INSERT INTO sondes (ip, name) VALUES ('" . $data['ip'] . "', '" . $data['name'] . "')";
+            $pdo->exec($sql);
+        } catch (PDOException $e) {
+            echo json_encode(['error' => $e->getMessage()]);
         }
+    }
 
+    public function put()
+    {
+        $data = json_decode(file_get_contents("php://input"), true);
+        try {
+            $pdo = getDatabaseConnection();
+            $sql = "UPDATE sondes SET ip='" . $data['ip'] . "', name='" . $data['name'] . "' WHERE id = '" . $data['id_sonde'] . "'";
+            $pdo->exec($sql);
+        } catch (PDOException $e) {
+            echo json_encode(['error' => $e->getMessage()]);
+        }
+    }
 
+    public function delete()
+    {
+        $data = json_decode(file_get_contents("php://input"), true);
+        try {
+            $pdo = getDatabaseConnection();
+            $sql = "DELETE FROM sondes WHERE id = " . $data['id'];
+            $pdo->exec($sql);
+        } catch (PDOException $e) {
+            echo json_encode(['error' => $e->getMessage()]);
+        }
     }
 }
